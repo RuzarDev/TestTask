630b52717273d9dab3988758e6545f2d6577691a
RuzarDev <urazov.ruzar@inbox.ru>
2025-08-01 18:21:05 +0500

    fix

--- project/src/utils/api.ts
+++ project/src/utils/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 enum ApiRoutes {
     LOCAL = 'http://localhost:3000',
+    RENDER = "https://testtaskserver.onrender.com"
 }
 
 export const api = axios.create({
-    baseURL: ApiRoutes.LOCAL,
+    baseURL: ApiRoutes.RENDER,
     headers: {
         'Content-Type': 'application/json',
     },
